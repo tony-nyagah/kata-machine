1a7c990616469b8a8065d6182acb0371c8643c8d
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-10-30 06:30:22 +0300

    implement binary search algorithm in bs_list function

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = middle;
+        } else {
+            low = middle + 1;
+        }
+    } while (low < high);
+
+    return false;
 }
